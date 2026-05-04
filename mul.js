fdd3ea87fce539173ed4f9b746c2514687f3d031
Harshitha <kharshi490@gmail.com>
2026-05-04 10:53:17 +0530

    initial

---
+++ mul.js
@@ -0,0 +1,29 @@
+var exp=require("express");
+var app=exp();
+var fs=require("fs");
+var count=0;
+app.get("/mul/:a/:b",(req,res)=>{
+if(req.params.a*req.params.b){
+    res.send(req.params.a*req.params.b)
+}
+else{
+    res.send("Invalid")
+}
+})
+app.get("/counter",(req,res)=>{
+res.send(count)
+})
+app.get("/counter/inc",(req,res)=>{
+    res.send(++count)
+})
+app.get("/counter/dec",(req,res)=>{
+    res.send(--count)
+})
+app.get("/products",(req,res)=>{
+    res.send("prod")
+    var data=fs.readFileSync("products.txt");
+
+})
+app.listen(3500,()=>{
+    console.log("server is Running");
+})
